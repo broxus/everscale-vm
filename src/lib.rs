41012e8ae2fa1835081561008f39e979f65c6176
Ivan Kalinin <i.kalinin@dexpa.io>
2023-09-27 18:57:18 +0200

    wip: Add StackValue

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,58 @@
+/// Prevents using `From::from` for plain error conversion.
+macro_rules! ok {
+    ($e:expr $(,)?) => {
+        match $e {
+            core::result::Result::Ok(val) => val,
+            core::result::Result::Err(err) => return core::result::Result::Err(err),
+        }
+    };
+}
+
 use std::rc::Rc;
 
+use everscale_types::error::Error;
 use everscale_types::prelude::*;
 use num_bigint::BigInt;
+
+use crate::util::{store_int_to_builder, OwnedCellSlice};
+
+mod util;
+
+#[derive(Default)]
+pub struct Stack {
+    pub itmes: Vec<RcStackValue>,
+}
+
+impl Stack {
+    pub fn make_null() -> RcStackValue {
+        thread_local! {
+            static NULL: RcStackValue = Rc::new(());
+        }
+        NULL.with(Rc::clone)
+    }
+
+    pub fn make_nan() -> RcStackValue {
+        thread_local! {
+            static NAN: RcStackValue = Rc::new(NaN);
+        }
+        NAN.with(Rc::clone)
+    }
+}
 
 pub trait StackValue {
     fn ty(&self) -> StackValueType;
 
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        context: &mut dyn CellContext,
+    ) -> Result<(), Error>;
+
     fn as_int(&self) -> Option<&BigInt> {
+        None
+    }
+
+    fn into_int(self: Rc<Self>) -> Option<Rc<BigInt>> {
         None
     }
 
@@ -14,22 +60,44 @@ pub trait StackValue {
         None
     }
 
+    fn into_cell(self: Rc<Self>) -> Option<Rc<Cell>> {
+        None
+    }
+
+    fn as_slice(&self) -> Option<&OwnedCellSlice> {
+        None
+    }
+
+    fn into_slice(self: Rc<Self>) -> Option<Rc<OwnedCellSlice>> {
+        None
+    }
+
     fn as_builder(&self) -> Option<&CellBuilder> {
         None
     }
 
-    fn as_slice(&self) -> Option<&CellSliceParts> {
+    fn into_builder(self: Rc<Self>) -> Option<Rc<CellBuilder>> {
         None
     }
 
-    fn as_cont(&self) -> Option<&Rc<dyn Continuation>> {
+    fn as_cont(&self) -> Option<&RcCont> {
         None
     }
 
-    fn as_tuple(&self) -> Option<&[Rc<dyn StackValue>]> {
+    fn into_cont(self: Rc<Self>) -> Option<Rc<RcCont>> {
+        None
+    }
+
+    fn as_tuple(&self) -> Option<&[RcStackValue]> {
+        None
+    }
+
+    fn into_tuple(self: Rc<Self>) -> Option<Rc<Vec<RcStackValue>>> {
         None
     }
 }
+
+pub type RcStackValue = Rc<dyn StackValue>;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum StackValueType {
@@ -37,8 +105,8 @@ pub enum StackValueType {
     NaN,
     Int,
     Cell,
-    Builder,
     Slice,
+    Builder,
     Cont,
     Tuple,
 }
@@ -46,6 +114,14 @@ pub enum StackValueType {
 impl StackValue for () {
     fn ty(&self) -> StackValueType {
         StackValueType::Null
+    }
+
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        _: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        builder.store_zeros(8)
     }
 }
 
@@ -56,6 +132,14 @@ impl StackValue for NaN {
     fn ty(&self) -> StackValueType {
         StackValueType::NaN
     }
+
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        _: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        builder.store_u16(0x02ff)
+    }
 }
 
 impl StackValue for BigInt {
@@ -63,7 +147,20 @@ impl StackValue for BigInt {
         StackValueType::Int
     }
 
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        _: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        ok!(builder.store_small_uint(0x0200 >> 1, 15));
+        store_int_to_builder(self, 257, true, builder)
+    }
+
     fn as_int(&self) -> Option<&BigInt> {
+        Some(self)
+    }
+
+    fn into_int(self: Rc<Self>) -> Option<Rc<BigInt>> {
         Some(self)
     }
 }
@@ -73,55 +170,140 @@ impl StackValue for Cell {
         StackValueType::Cell
     }
 
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        _: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        ok!(builder.store_u8(0x03));
+        builder.store_reference(self.clone())
+    }
+
     fn as_cell(&self) -> Option<&Cell> {
+        Some(self)
+    }
+
+    fn into_cell(self: Rc<Self>) -> Option<Rc<Cell>> {
         Some(self)
     }
 }
 
+impl StackValue for OwnedCellSlice {
+    fn ty(&self) -> StackValueType {
+        StackValueType::Slice
+    }
+
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        _: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        // TODO: add flag
+        ok!(builder.store_u8(0x04));
+        ok!(builder.store_reference(self.cell().clone()));
+
+        let range = self.range();
+        let value = ((range.bits_offset() as u64) << 16)
+            | (((range.bits_offset() + range.remaining_bits()) as u64) << 6)
+            | ((range.refs_offset() as u64) << 3)
+            | (range.refs_offset() + range.remaining_refs()) as u64;
+        builder.store_uint(value, 26)
+    }
+
+    fn as_slice(&self) -> Option<&OwnedCellSlice> {
+        Some(self)
+    }
+
+    fn into_slice(self: Rc<Self>) -> Option<Rc<OwnedCellSlice>> {
+        Some(self)
+    }
+}
 impl StackValue for CellBuilder {
     fn ty(&self) -> StackValueType {
         StackValueType::Builder
     }
 
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        context: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        ok!(builder.store_u8(0x05));
+        builder.store_reference(ok!(self.clone().build_ext(context)))
+    }
+
     fn as_builder(&self) -> Option<&CellBuilder> {
         Some(self)
     }
-}
 
-impl StackValue for CellSliceParts {
-    fn ty(&self) -> StackValueType {
-        StackValueType::Slice
-    }
-
-    fn as_slice(&self) -> Option<&CellSliceParts> {
+    fn into_builder(self: Rc<Self>) -> Option<Rc<CellBuilder>> {
         Some(self)
     }
 }
 
-impl StackValue for Rc<dyn Continuation> {
+impl StackValue for RcCont {
     fn ty(&self) -> StackValueType {
         StackValueType::Cont
     }
 
-    fn as_cont(&self) -> Option<&Rc<dyn Continuation>> {
+    fn as_cont(&self) -> Option<&Rc<dyn Cont>> {
+        Some(self)
+    }
+
+    fn into_cont(self: Rc<Self>) -> Option<Rc<RcCont>> {
         Some(self)
     }
 }
 
-impl StackValue for Vec<Rc<dyn StackValue>> {
+impl StackValue for Vec<RcStackValue> {
     fn ty(&self) -> StackValueType {
         StackValueType::Tuple
     }
 
-    fn as_tuple(&self) -> Option<&[Rc<dyn StackValue>]> {
+    fn store_as_stack_value(
+        &self,
+        builder: &mut CellBuilder,
+        context: &mut dyn CellContext,
+    ) -> Result<(), Error> {
+        if self.len() > u16::MAX as usize {
+            return Err(Error::IntOverflow);
+        }
+
+        let mut head = None::<Cell>;
+        let mut tail = None::<Cell>;
+        let mut result = CellBuilder::new();
+
+        for item in self {
+            std::mem::swap(&mut head, &mut tail);
+
+        }
+
+        ok!(builder.store_u8(0x07));
+        ok!(builder.store_u16(self.len() as _));
+        if let Some(head) = head {
+            ok!(builder.store_reference(head));
+        }
+        if let Some(tail) = tail {
+            ok!(builder.store_reference(tail));
+        }
+        Ok(())
+    }
+
+    fn as_tuple(&self) -> Option<&[RcStackValue]> {
+        Some(self)
+    }
+
+    fn into_tuple(self: Rc<Self>) -> Option<Rc<Vec<RcStackValue>>> {
         Some(self)
     }
 }
 
-pub trait Continuation {}
+pub trait Cont {}
+
+pub type RcCont = Rc<dyn Cont>;
 
 pub struct ControlRegs {
-    pub c: [Rc<dyn Continuation>; Self::CONT_REG_COUNT],
+    pub c: [Rc<dyn Cont>; Self::CONT_REG_COUNT],
     pub d: [Cell; Self::DATA_REG_COUNT],
 }
 
@@ -138,27 +320,27 @@ pub struct ExtQuitCont;
 
 pub struct PushIntCont {
     pub value: i32,
-    pub next: Rc<dyn Continuation>,
+    pub next: Rc<dyn Cont>,
 }
 
 pub struct RepeatCont {
     pub count: u64,
-    pub body: Rc<dyn Continuation>,
-    pub after: Rc<dyn Continuation>,
+    pub body: Rc<dyn Cont>,
+    pub after: Rc<dyn Cont>,
 }
 
 pub struct AgainCont {
-    pub body: Rc<dyn Continuation>,
+    pub body: Rc<dyn Cont>,
 }
 
 pub struct UntilCont {
-    pub body: Rc<dyn Continuation>,
-    pub after: Rc<dyn Continuation>,
+    pub body: Rc<dyn Cont>,
+    pub after: Rc<dyn Cont>,
 }
 
 pub struct WhileCont {
-    pub cont: Rc<dyn Continuation>,
-    pub body: Rc<dyn Continuation>,
-    pub after: Rc<dyn Continuation>,
+    pub cont: Rc<dyn Cont>,
+    pub body: Rc<dyn Cont>,
+    pub after: Rc<dyn Cont>,
     pub check_cond: bool,
 }
---
+++ src/util.rs
@@ -0,0 +1,157 @@
+use std::sync::OnceLock;
+
+use everscale_types::error::Error;
+use everscale_types::prelude::*;
+use num_bigint::{BigInt, BigUint, Sign};
+use num_traits::{One, ToPrimitive, Zero};
+
+#[derive(Clone)]
+#[repr(transparent)]
+pub struct OwnedCellSlice(CellSliceParts);
+
+impl OwnedCellSlice {
+    pub fn new(cell: Cell) -> Self {
+        let range = CellSliceRange::full(cell.as_ref());
+        Self((cell, range))
+    }
+
+    pub fn apply(&self) -> Result<CellSlice<'_>, Error> {
+        self.range().apply(self.cell())
+    }
+
+    #[inline]
+    pub fn range(&self) -> CellSliceRange {
+        self.0 .1
+    }
+
+    #[inline]
+    pub fn cell(&self) -> &Cell {
+        &self.0 .0
+    }
+
+    #[inline]
+    pub fn set_range(&mut self, range: CellSliceRange) {
+        self.0 .1 = range
+    }
+}
+
+impl From<CellSliceParts> for OwnedCellSlice {
+    #[inline]
+    fn from(parts: CellSliceParts) -> Self {
+        Self(parts)
+    }
+}
+
+impl PartialEq<CellSlice<'_>> for OwnedCellSlice {
+    fn eq(&self, right: &CellSlice<'_>) -> bool {
+        if let Ok(left) = self.apply() {
+            if let Ok(std::cmp::Ordering::Equal) = left.cmp_by_content(right) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+pub fn store_int_to_builder(
+    int: &BigInt,
+    bits: u16,
+    signed: bool,
+    builder: &mut CellBuilder,
+) -> Result<(), Error> {
+    let value = int.magnitude();
+    if value.bits() > bits as u64 {
+        return Err(Error::IntOverflow);
+    }
+
+    let is_negative = int.sign() == Sign::Minus;
+    let bytes = to_signed_bytes_be(is_negative, value);
+    let value_bits = (bytes.len() * 8) as u16;
+
+    if bytes.is_empty() {
+        builder.store_zeros(bits)
+    } else if bits > value_bits {
+        let diff = bits - value_bits;
+        ok!(if is_negative {
+            builder.store_ones(diff)
+        } else {
+            builder.store_zeros(diff)
+        });
+        builder.store_raw(&bytes, value_bits)
+    } else {
+        let bits_offset = value_bits - bits;
+        let bytes_offset = (bits_offset / 8) as usize;
+        let rem = bits_offset % 8;
+
+        let (left, right) = bytes.split_at(bytes_offset + 1);
+        if let Some(left) = left.last() {
+            ok!(builder.store_small_uint(*left << rem, 8 - rem));
+        }
+        if !right.is_empty() {
+            ok!(builder.store_raw(right, (right.len() * 8) as u16));
+        }
+        Ok(())
+    }
+}
+
+pub fn bitsize(int: &BigInt, signed: bool) -> u16 {
+    fn minus_one() -> &'static BigInt {
+        static MINUS_ONE: OnceLock<BigInt> = OnceLock::new();
+        MINUS_ONE.get_or_init(|| BigInt::from_biguint(Sign::Minus, BigUint::one()))
+    }
+
+    let mut bits = int.bits() as u16;
+    if signed {
+        if int.is_zero() || int == minus_one() {
+            return 1;
+        } else if int.sign() == Sign::Plus {
+            return bits + 1;
+        }
+
+        let mut modpow2 = int.magnitude().clone();
+        modpow2 &= &modpow2 - 1u32;
+        if !modpow2.is_zero() {
+            bits += 1;
+        }
+    }
+
+    bits
+}
+
+pub fn to_signed_bytes_be(is_negative: bool, value: &BigUint) -> Vec<u8> {
+    #[inline]
+    fn is_zero(value: &u8) -> bool {
+        *value == 0
+    }
+
+    #[inline]
+    pub fn twos_complement_le(digits: &mut [u8]) {
+        let mut carry = true;
+        for digit in digits {
+            *digit = !*digit;
+            if carry {
+                let (d, c) = digit.overflowing_add(1);
+                *digit = d;
+                carry = c;
+            }
+        }
+    }
+
+    fn negative_to_signed_bytes_be(value: &BigUint) -> Vec<u8> {
+        let mut bytes = value.to_bytes_le();
+        let last_byte = bytes.last().cloned().unwrap_or(0);
+        if last_byte > 0x7f && !(last_byte == 0x80 && bytes.iter().rev().skip(1).all(is_zero)) {
+            // msb used by magnitude, extend by 1 byte
+            bytes.push(0);
+        }
+        twos_complement_le(&mut bytes);
+        bytes.reverse();
+        bytes
+    }
+
+    if is_negative {
+        negative_to_signed_bytes_be(value)
+    } else {
+        value.to_bytes_be()
+    }
+}
