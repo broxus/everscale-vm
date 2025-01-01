423a444bc0af5afea7dee45a05fd0fca6c02700e
Ivan Kalinin <i.kalinin@dexpa.io>
2025-01-01 19:45:38 +0100

    Add simple benches

---
+++ benches/ever_wallet.rs
@@ -0,0 +1,53 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use everscale_types::boc::Boc;
+use everscale_types::models::{CurrencyCollection, IntAddr, OwnedMessage};
+use everscale_vm::{tuple, GasParams, SmcInfoBase, VmState};
+
+fn vm_benchmark(c: &mut Criterion) {
+    let code = Boc::decode_base64("te6cckEBBgEA/AABFP8A9KQT9LzyyAsBAgEgAgMABNIwAubycdcBAcAA8nqDCNcY7UTQgwfXAdcLP8j4KM8WI88WyfkAA3HXAQHDAJqDB9cBURO68uBk3oBA1wGAINcBgCDXAVQWdfkQ8qj4I7vyeWa++COBBwiggQPoqFIgvLHydAIgghBM7mRsuuMPAcjL/8s/ye1UBAUAmDAC10zQ+kCDBtcBcdcBeNcB10z4AHCAEASqAhSxyMsFUAXPFlAD+gLLaSLQIc8xIddJoIQJuZgzcAHLAFjPFpcwcQHLABLM4skB+wAAPoIQFp4+EbqOEfgAApMg10qXeNcB1AL7AOjRkzLyPOI+zYS/").unwrap();
+    let data = Boc::decode_base64(
+        "te6ccgEBAQEAKgAAUMiw1sYIywOsoSiX5hi1n13b0Qt+KVhFxHpNZ3ILXjV1AAABk0YeykY=",
+    )
+    .unwrap();
+
+    let message_cell = Boc::decode_base64("te6ccgEBBAEA0gABRYgAxgNljqstzcrTTaJ1ydjhkp4u/ZwXwz8tG7nOeonPX44MAQHhmt2/xQjjwjfYraY7Tv53Ct8o9OAtI8nD7DFB19TrG7W8wYMxQKtbXuvGvaKFoB9D0lMZwnPpZ1fEBWxaXZgtg/IsNbGCMsDrKEol+YYtZ9d29ELfilYRcR6TWdyC141dQAAAZNGIEb+Zzz2EEzuZGyACAWWADGA2WOqy3NytNNonXJ2OGSni79nBfDPy0buc56ic9fjgAAAAAAAAAAAAAAAHc1lAADgDAAA=").unwrap();
+    let message = message_cell.parse::<OwnedMessage>().unwrap();
+
+    let addr = "0:6301b2c75596e6e569a6d13ae4ec70c94f177ece0be19f968ddce73d44e7afc7"
+        .parse::<IntAddr>()
+        .unwrap();
+
+    c.bench_function("ever_wallet", |b| {
+        b.iter(|| {
+            let smc_info = SmcInfoBase::new()
+                .with_now(1732048342)
+                .with_block_lt(55398352000001)
+                .with_tx_lt(55398317000004)
+                .with_account_balance(CurrencyCollection::new(10000000000))
+                .with_account_addr(addr.clone())
+                .require_ton_v4()
+                .with_code(code.clone());
+
+            let mut vm_state = VmState::builder()
+                .with_smc_info(smc_info)
+                .with_stack(tuple![
+                    int 4989195982u64,
+                    int 0,
+                    cell message_cell.clone(),
+                    slice message.body.clone(),
+                    int -1,
+                ])
+                .with_code(code.clone())
+                .with_data(data.clone())
+                .with_gas(GasParams::getter())
+                .build()
+                .unwrap();
+
+            let result = vm_state.run();
+            _ = black_box(result);
+        });
+    });
+}
+
+criterion_group!(benches, vm_benchmark);
+criterion_main!(benches);
---
+++ benches/jetton.rs
@@ -0,0 +1,46 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use everscale_types::boc::Boc;
+use everscale_types::cell::CellBuilder;
+use everscale_types::models::{CurrencyCollection, IntAddr};
+use everscale_vm::{tuple, GasParams, SmcInfoBase, VmState};
+
+fn vm_benchmark(c: &mut Criterion) {
+    let code = Boc::decode_base64("te6ccgECGgEABQ4AART/APSkE/S88sgLAQIBYgIDAgLLBAUCASAQEQHX0MtDTAwFxsI5EMIAg1yHTHwGCEBeNRRm6kTDhgEDXIfoAMO1E0PoA+kD6QNTU0/8B+GHRUEWhQTT4QchQBvoCUATPFljPFszMy//J7VTg+kD6QDH6ADH0AfoAMfoAATFw+DoC0x8BAdM/ARKBgAdojhkZYOA54tkgUGD+gvABPztRND6APpA+kDU1NP/Afhh0SaCEGQrfQe6jss1NVFhxwXy4EkE+kAh+kQwwADy4U36ANTRINDTHwGCEBeNRRm68uBIgEDXIfoA+kAx+kAx+gAg1wsAmtdLwAEBwAGw8rGRMOJUQxvgOSWCEHvdl9664wIlghAsdrlzuuMCNCQHCAkKAY4hkXKRceL4OSBuk4F4LpEg4iFulDGBfuCRAeJQI6gToHOBBK1w+DygAnD4NhKgAXD4NqBzgQUTghAJZgGAcPg3oLzysCVZfwsB5jUF+gD6QPgo+EEoEDQB2zxvIjD5AHB0yMsCygfL/8nQUAjHBfLgShKhRBRQNvhByFAG+gJQBM8WWM8WzMzL/8ntVPpA0SDXCwHAALOOIsiAEAHLBQHPFnD6AnABy2qCENUydtsByx8BAcs/yYBC+wCRW+IYAdI1XwM0AfpA0gABAdGVyCHPFsmRbeLIgBABywVQBM8WcPoCcAHLaoIQ0XNUAAHLH1AEAcs/I/pEMMAAjp34KPhBEDVBUNs8byIw+QBwdMjLAsoHy//J0BLPFpcxbBJwAcsB4vQAyYBQ+wAYBP6CEGUB81S6jiUwM1FCxwXy4EkC+kDRQAME+EHIUAb6AlAEzxZYzxbMzMv/ye1U4CSCEPuI4Rm6jiQxMwPRUTHHBfLgSYsCQDT4QchQBvoCUATPFljPFszMy//J7VTgJIIQy4YpArrjAjAjghAlCNZquuMCI4IQdDHyIbrjAhA2DA0ODwHAghA7msoAcPsC+Cj4QRA2QVDbPG8iMCD5AHB0yMsCygfL/8iAGAHLBQHPF1j6AgKYWHdQA8trzMyXMAFxWMtqzOLJgBH7AFAFoEMU+EHIUAb6AlAEzxZYzxbMzMv/ye1UGABONDZRRccF8uBJyFADzxbJEDQS+EHIUAb6AlAEzxZYzxbMzMv/ye1UACI2XwMCxwXy4EnU1NEB7VT7BABKM1BCxwXy4EkB0YsCiwJANPhByFAG+gJQBM8WWM8WzMzL/8ntVAAcXwaCENNyFYy63IQP8vACAUgSEwICcRYXAT+10V2omh9AH0gfSBqamn/gPww6IovgnwUfCCJbZ43kUBgCAWoUFQAuq1vtRND6APpA+kDU1NP/Afhh0RAkXwQALqpn7UTQ+gD6QPpA1NTT/wH4YdFfBfhBAVutvPaiaH0AfSB9IGpqaf+A/DDoii+CfBR8IIltnjeRGHyAODpkZYFlA+X/5OhAGACLrxb2omh9AH0gfSBqamn/gPww6L+Z6DbBeDhy69tRTZyXwoO38K5ReQKeK2EZw5RicZ5PRu2PdBPmLHgKOGRlg/oAZKGAQAH2hA9/cCb6RDGr+1MRSUYYBMjLA1AD+gIBzxYBzxbL/yCBAMrIyw8Bzxck+QAl12UlggIBNMjLFxLLD8sPy/+OKQakXAHLCXH5BABScAHL/3H5BACr+yiyUwS5kzQ0I5Ew4iDAICTAALEX5hAjXwMzMyJwA8sJySLIywESGQAU9AD0AMsAyQFvAg==").unwrap();
+    let data = Boc::decode_base64(
+            "te6ccgEBBAEA3gACTmE+QBlNGKCvtRVlwuLLP8LwzhcDJNm1TPewFBFqmlIYet7ln0NupwECCEICDvGeG/QPK6SS/KrDhu7KWb9oJ6OFBwjZ/NmttoOrwzYB5mh0dHBzOi8vZ2lzdC5naXRodWJ1c2VyY29udGVudC5jb20vRW1lbHlhbmVua29LLzI3MWMwYWRhMWRlNDJiOTdjNDU1YWM5MzVjOTcyZjQyL3Jhdy9iN2IzMGMzZTk3MGUwNzdlMTFkMDg1Y2M2NzEzYmUDADAzMTU3YzdjYTA4L21ldGFkYXRhLmpzb24=",
+        ).unwrap();
+
+    let addr = "0:2a0c78148c73416b63250b990efdfbf9d5897bf3b33e2f5498a2fe0617174bb8"
+        .parse::<IntAddr>()
+        .unwrap();
+
+    c.bench_function("jetton", |b| {
+        b.iter(|| {
+            let smc_info = SmcInfoBase::new()
+                .with_now(1733142533)
+                .with_block_lt(50899537000013)
+                .with_tx_lt(50899537000013)
+                .with_account_balance(CurrencyCollection::new(1931553923))
+                .with_account_addr(addr.clone())
+                .require_ton_v4();
+
+            let mut vm_state = VmState::builder()
+                .with_smc_info(smc_info)
+                .with_stack(tuple![
+                    slice CellBuilder::build_from(&addr).unwrap(),
+                    int 103289,
+                ])
+                .with_code(code.clone())
+                .with_data(data.clone())
+                .with_gas(GasParams::getter())
+                .build()
+                .unwrap();
+
+            let result = vm_state.run();
+            _ = black_box(result);
+        });
+    });
+}
+
+criterion_group!(benches, vm_benchmark);
+criterion_main!(benches);
